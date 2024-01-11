16b52d917832cc6bb7f83eb0644f80043ad32aa3
Rahil Kothari <kotharirahil9@gmail.com>
2024-01-11 20:23:56 +0530

    Add firebase and cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Background } from './components/Background'
 import QuestionGenerator from './components/QuestionGenerator'
 
 function App() {
@@ -10,7 +7,6 @@ function App() {
 
   return (
     <>
-      {/* <Background/> */}
       <QuestionGenerator></QuestionGenerator>
     </>
   )
