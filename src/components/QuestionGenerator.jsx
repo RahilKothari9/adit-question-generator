be3ee413008b1bee65170b479a4981a8c83a13d1
Rahil Kothari <kotharirahil9@gmail.com>
2024-01-11 20:43:25 +0530

    Update prompt

--- src/components/QuestionGenerator.jsx
+++ src/components/QuestionGenerator.jsx
@@ -6,7 +6,6 @@ import Button from '@mui/material/Button';
 const generationConfig = {
     maxOutputTokens: 120,
     temperature: 1,
-    topP: 0.95,
     topK: 35,
   };
 
@@ -23,26 +22,18 @@ function QuestionGenerator() {
       
         const prompt = `Generate 1 single question, which I can use as a conversation starter with family or friends. 
         Some example questions are 
-        1. "What is the most unexpected act of kindness you've witnessed or experienced in recent memory?"
-
-        2. "If you had the power to instantly master a new skill or knowledge set, what would you choose and why?"
         
-        3. Have you ever had a gut feeling or intuitive hunch turned out to be remarkably accurate? Share a story about it.
-        
-        4.  Who taught you most about yourself in the last year?
+        1.  Who taught you most about yourself in the last year?
 
-        5. What fruit represents your travel self?
+        2. What fruit represents your travel self?
 
-        6. When do you feel most at home?
+        3. When do you feel most at home?
 
-        7. Why do you over think your last big decision?
-
-        8. What reminds you most of your favorite dessert?
-
-        Return a single question with no formatting and having a maximum of 40 words. Make the language a informal.
-
-
-        
+        Speak casually
+        Return a single question with no formatting and having a maximum of 50 words and minimum of 20.
+        DO NOT end your sentences with who would it be and why or phrases of that nature, generally ending in and why or how.
+        Make the questions exciting, dont address people with phrases like my friends, hey everyone etc.
+        Ask about experiences, personal milestones, personal events, preferences, choices .
         
         `;
       
